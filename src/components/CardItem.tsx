f212fb4bfa377c1812bf074e8597cdb652ce45a3
Solanki Gaurav <solankigaurav2107@gmail.com>
2024-12-01 13:00:28 +0530

    add loader

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+const Spinner = process.env.PUBLIC_URL + "/loader.gif";
 interface Props {
   heading: string;
   image: string;
   onImageSelect: () => void;
 }
-export default function CardItem({heading, image, onImageSelect}: Props) {
+export default function CardItem({ heading, image, onImageSelect }: Props) {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div className="card">
-      <img src={image} alt={heading} onClick={() => onImageSelect()}/>
+      <img
+        className={"card-img"}
+        src={loading ? Spinner : image}
+        alt={heading}
+        onClick={() => onImageSelect()}
+        onLoad={() => setLoading(false)}
+      />
       <div className="card-content">
         <h2 className="card-title">{heading}</h2>
       </div>
